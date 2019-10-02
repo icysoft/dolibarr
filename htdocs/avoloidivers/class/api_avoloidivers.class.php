e506d8573fc35b7454d0ceae1c005dd1ad3cce51
francois <francois@icysoft.fr>
2019-10-02 12:52:25 +0200

    Initialisation du module de recherche de tiers

---
+++ htdocs/avoloidivers/class/api_avoloidivers.class.php
@@ -0,0 +1,72 @@
+<?php
+/* Copyright (C) 2016   Xebax Christy           <xebax@wanadoo.fr>
+ * Copyright (C) 2016	Laurent Destailleur		<eldy@users.sourceforge.net>
+ * Copyright (C) 2016   Jean-François Ferry     <jfefe@aternatik.fr>
+ *
+ * This program is free software you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use Luracast\Restler\RestException;
+use Luracast\Restler\Format\UploadFormat;
+
+
+require_once DOL_DOCUMENT_ROOT.'/main.inc.php';
+
+/**
+ * API class for receive files
+ *
+ * @access protected
+ * @class AvoloiDivers {@requires user,external}
+ */
+class AvoloiDivers extends DolibarrApi
+{
+
+	/**
+	 * @var array   $DOCUMENT_FIELDS     Mandatory fields, checked when create and update object
+	 */
+	static $DOCUMENT_FIELDS = array(
+		'modulepart'
+	);
+
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+		global $db;
+		$this->db = $db;
+	}
+
+
+	/**
+	 * Search a tiers by it's name
+	 * 
+	 * @param   string   $searched
+	 * @return  array                   List of documents
+	 *
+	 * @throws 500
+	 * @throws 501
+	 * @throws 400
+	 * @throws 401
+	 * @throws 404
+	 * @throws 200
+	 *
+	 * @url GET /searchtiers
+	 */
+	public function searchtiers($searched) {
+		global $conf, $langs, $user;
+
+		
+	}
+}
